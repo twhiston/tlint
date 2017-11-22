719bb90e6bcbf04f9d2f49770408722e77625d61
Tom Whiston <tomw@nine.ch>
2017-11-22 21:02:55 +0100

    some refactoring

--- README.md
+++ README.md
@@ -1,15 +1,46 @@
 # tlint
 
-My own dumb linting program, you probably shouldnt use this.
-Its really inflexible, not configurable at all etc...
+My own dumb linting program, ties together a few common linters with some config files
+to do common things in projects.
+
+YOU MUST INSTALL THE LINTERS YOURSELF!
+This is just glue to put them together with a config file
 
 Runs:
 
 - goimports
+- go fmt
 - gometalinter
 - hadolint
 - shellcheck
 - checkmake
 
-over everything under the cwd.
+recursively under the cwd.
 It will glob appropriately for the tools requirements
+
+## Config
+
+Support simple configuration via a `.tlint.yml` file. This can be a global config in the home dir
+or a per project config. Per project config will be prefered to global.
+
+- goimports       - none
+- go fmt          - none
+- gometalinter    - .gometalinter.json in cwd
+                    In the format of the config struct, which will be passed as the --config option
+                    https://github.com/alecthomas/gometalinter/blob/master/config.go
+- hadolint        - as hadolint does not support a config file config value may be placed in the .tlint.yml file
+
+    ```
+hadolint:
+    ignore:
+        - DL3007
+```
+- shellcheck      - use annotations in your scripts or include ignores in the config file
+    ```
+shellcheck:
+    ignore:
+        - SC2034
+```
+- checkmake       - .checkmake.ini in cwd
+                    https://github.com/mrtazz/checkmake/blob/master/fixtures/exampleConfig.ini
+                    https://github.com/mrtazz/checkmake/blob/master/man/man1/checkmake.1.md
--- cmd/root.go
+++ cmd/root.go
@@ -18,20 +18,30 @@ import (
 	"fmt"
 	"os"
 
-	"log"
-
 	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	"log"
 )
 
 var cfgFile string
+var localConfPath string
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "tlint",
-	Short: "runs the linters recursively within cwd",
-	Long:  ``,
+	Short: "runs linters recursively within cwd",
+	Long: `
+All linters enabled in a .tlint.yml file will be run over the current cwd and all directories below.
+If a .tlint.yml file cannot be found tlint will look for $HOME/.tlint.yml
+
+Config file will be overriden by options. For example
+	tlint -m=false
+ensures that gometalinter is never run and
+	tlint -d
+ensures that hadolint is always run
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		log.SetFlags(0)
@@ -40,44 +50,85 @@ var RootCmd = &cobra.Command{
 		checkError(err)
 
 		//goimports
-		files, err := globExt(dir, ".go")
-		checkError(err)
-		for _, v := range files {
-			runcmd("goimports", "-w", v)
+		if viper.GetBool("status.imports") {
+			files, err := globExt(dir, ".go")
+			checkError(err)
+			runArgsCmd("goimports", files, []string{"-w"})
+		}
+
+		//gofmt
+		if viper.GetBool("status.fmt") {
+			files, err := globExt(dir, ".go")
+			checkError(err)
+			runArgsCmd("go", files, []string{"fmt"})
 		}
 
 		//gometalinter
-		runcmd("gometalinter", "./...")
+		if viper.GetBool("status.gometalinter") {
+			cmdArgs := []string{
+				"./...",
+			}
+			if _, err := os.Stat(localConfPath + ".gometalinter.json"); !os.IsNotExist(err) {
+				cmdArgs = append(cmdArgs, "--config", localConfPath+"gometalinter.json")
+			}
+			runcmd("gometalinter", cmdArgs...)
+		}
 
 		//hadolint
-		files, err = glob(dir, "Dockerfile")
-		checkError(err)
-		for _, v := range files {
-			runcmd("hadolint", v)
+		if viper.GetBool("status.hadolint") {
+			files, err := glob(dir, "Dockerfile")
+			checkError(err)
+			cmdArgs := buildCmdArgs("hadolint.ignore", "--ignore")
+			runArgsCmd("hadolint", files, cmdArgs)
 		}
 
 		//shellcheck
-		files, err = globExt(dir, ".sh")
-		checkError(err)
-		for _, v := range files {
-			runcmd("shellcheck", v)
+		if viper.GetBool("status.shellcheck") {
+			files, err := globExt(dir, ".sh")
+			checkError(err)
+			cmdArgs := buildCmdArgs("shellcheck.ignore", "-e")
+			runArgsCmd("shellcheck", files, cmdArgs)
 		}
 
 		//shellcheck every file in a bin directory in the image, as these are usually shell scripts
 		//Could be better!
-		files, err = getDirContents(dir, "bin")
-		checkError(err)
-		for _, v := range files {
-			runcmd("shellcheck", v)
+		if viper.GetBool("status.shellcheck_bin") {
+			files, err := getDirContents(dir, "bin")
+			checkError(err)
+			cmdArgs := buildCmdArgs("shellcheck.ignore", "-e")
+			runArgsCmd("shellcheck", files, cmdArgs)
 		}
 
 		//checkmake
-		files, err = glob(dir, "Makefile")
-		checkError(err)
-		for _, v := range files {
-			runcmd("checkmake", v)
+		if viper.GetBool("status.checkmake") {
+			files, err := glob(dir, "Makefile")
+			checkError(err)
+
+			var cmdArgs []string
+			if _, err := os.Stat(localConfPath + ".checkmake.ini"); !os.IsNotExist(err) {
+				cmdArgs = append(cmdArgs, "--config="+localConfPath+".checkmake.ini")
+			}
+			runArgsCmd("checkmake", files, cmdArgs)
 		}
 	},
+}
+
+func buildCmdArgs(sliceKey string, optionKey string) []string {
+	var cmdArgs []string
+	cmdData := viper.GetStringSlice(sliceKey)
+	if len(cmdData) > 0 {
+		for _, v := range cmdData {
+			cmdArgs = append(cmdArgs, optionKey, v)
+		}
+	}
+	return cmdArgs
+}
+
+func runArgsCmd(cmd string, files []string, cmdArgs []string) {
+	for _, v := range files {
+		fileArgs := append(cmdArgs, v)
+		runcmd(cmd, fileArgs...)
+	}
 }
 
 func checkError(err error) {
@@ -96,6 +147,7 @@ func Execute() {
 }
 
 func init() {
+	log.SetFlags(0)
 	cobra.OnInitialize(initConfig)
 
 	// Here you will define your flags and configuration settings.
@@ -105,25 +157,40 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.Flags().BoolP("imports", "i", true, "fix go imports")
+	RootCmd.Flags().BoolP("fmt", "f", true, "fix go formatting")
+	RootCmd.Flags().BoolP("gometalinter", "m", true, "run gometalinter")
+	RootCmd.Flags().BoolP("hadolint", "d", true, "run hadolint")
+	RootCmd.Flags().BoolP("shellcheck", "s", true, "run shellcheck")
+	RootCmd.Flags().BoolP("shellcheck-bin", "b", true, "run shellcheck on ANY file in a folder called bin, useful for s2i image linting")
+	RootCmd.Flags().BoolP("checkmake", "c", true, "run checkmake")
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
+
+	configName := ".tlint"
+
+	dir, err := os.Getwd()
+	checkError(err)
+	localConfPath = dir + "/"
+
 	if cfgFile != "" {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
 
-		// Search config in home directory with name ".tlint" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".tlint")
+		localConf := localConfPath + configName + ".yml"
+		if _, err := os.Stat(localConf); os.IsNotExist(err) {
+			// Find home directory.
+			home, err := homedir.Dir()
+			checkError(err)
+			// Search config in home directory with name ".tlint" (without extension).
+			viper.AddConfigPath(home)
+			viper.SetConfigName(configName)
+		} else {
+			viper.SetConfigFile(localConf)
+		}
 	}
 
 	viper.SetEnvPrefix("TL_")
@@ -132,5 +199,25 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	} else {
+		viper.Set("status.imports", true)
+		viper.Set("status.fmt", true)
+		viper.Set("status.gometalinter", true)
+		viper.Set("status.hadolint", true)
+		viper.Set("status.shellcheck", true)
+		viper.Set("status.shellcheck_bin", true)
+		viper.Set("status.checkmake", true)
 	}
+
+	ftests := []string{"imports", "fmt", "gometalinter", "hadolint", "shellcheck", "shellcheck-bin", "checkmake"}
+
+	for _, v := range ftests {
+		if RootCmd.Flags().Changed(v) {
+			log.Println("Config overriden by option:", v)
+			val, err := RootCmd.Flags().GetBool(v)
+			checkError(err)
+			viper.Set("status."+v, val)
+		}
+	}
+
 }
